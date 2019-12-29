18b4d6db2e2c4a79c04a23561e529b8298362642
datdn <datdn@ominext.com>
2019-12-29 11:23:05 +0700

    Initial Commit

---
+++ GithubActionsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubActionsTest
+//
+//  Created by Doan Dat on 12/29/19.
+//  Copyright © 2019 Doan Dat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
